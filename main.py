b530b06ca51d6ce66fcc07471e0ff9fbf715ab43
BekenRey <bekenvahuerrey32@gmail.com>
2025-08-24 10:00:52 -0500

    Initial commit: Set up blog project structure

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/proyectos")
+def proyectos():
+    return render_template("proyectos.html")
+
+@app.route("/videos")
+def videos():
+    return render_template("videos.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
